329297f18b75c4017b30c529afe7badc270907b8
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-02-05 12:12:09 +0100

    Features: dockerTemplate, Integration Test, Maven CI build

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,30 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 1/20/16.
+ */
+@Controller
+public class IndexController {
+
+    private ProductService productService;
+
+    @Autowired
+    public IndexController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping({"/", "index"})
+    public String getIndex(Model model){
+
+        model.addAttribute("products", productService.listProducts());
+
+        return "index";
+    }
+
+}
