9e11da71556d1d9a76140fdfec7cc5270d697d5a
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-02-05 12:49:07 +0100

    updated pageviewService

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,28 +1,46 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.events.PageViewEvent;
+import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by jt on 1/20/16.
  */
+@Slf4j
 @Controller
 public class IndexController {
 
     private ProductService productService;
+    private PageViewService pageViewService;
 
     @Autowired
-    public IndexController(ProductService productService) {
+    public IndexController(ProductService productService, PageViewService pageViewService) {
         this.productService = productService;
+        this.pageViewService = pageViewService;
     }
 
     @RequestMapping({"/", "index"})
     public String getIndex(Model model){
 
         model.addAttribute("products", productService.listProducts());
+
+        //Send page view event
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageUrl("springframework.guru/");
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+        log.info("Sending Message to page view service");
+        pageViewService.sendPageViewEvent(pageViewEvent);
 
         return "index";
     }
--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -1,11 +1,15 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.events.PageViewEvent;
 import guru.springframework.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by jt on 1/20/16.
@@ -24,6 +28,12 @@ public class ProductController {
     public String getProductById(@PathVariable Integer id, Model model){
 
         model.addAttribute("product", productService.getProduct(id));
+
+        //Send page view event
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageUrl("springframework.guru/" + id);
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
 
         return "product";
     }
